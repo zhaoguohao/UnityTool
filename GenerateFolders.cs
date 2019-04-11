8f5c89308827fd9dfcdd1fd3e7f8c97628a37bae
zhaoguohao <43693203+zhaoguohao@users.noreply.github.com>
2019-04-11 15:47:27 +0800

    GenerateFolders
    Tool生成通用的Unity存放资源脚本的文件夹

---
+++ GenerateFolders.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class GenerateFolders : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [MenuItem("Tools/CreateBasicFolder #&_b")]
+    private static void CreateBasicFolder()
+    {
+        GenerateFolder();
+        Debug.Log("Folders Created");
+    }
+
+    [MenuItem("Tools/CreateALLFolder")]
+    private static void CreateAllFolder()
+    {
+        GenerateFolder(1);
+        Debug.Log("Folders Created");
+    }
+
+
+    private static void GenerateFolder(int flag = 0)
+    {
+        // 文件路径
+        string prjPath = Application.dataPath + "/";
+        Directory.CreateDirectory(prjPath + "Audio");
+        Directory.CreateDirectory(prjPath + "Prefabs");
+        Directory.CreateDirectory(prjPath + "Materials");
+        Directory.CreateDirectory(prjPath + "Resources");
+        Directory.CreateDirectory(prjPath + "Scripts");
+        Directory.CreateDirectory(prjPath + "Textures");
+        Directory.CreateDirectory(prjPath + "Scenes");
+
+        if (1 == flag)
+        {
+            Directory.CreateDirectory(prjPath + "Meshes");
+            Directory.CreateDirectory(prjPath + "Shaders");
+            Directory.CreateDirectory(prjPath + "GUI");
+        }
+
+        AssetDatabase.Refresh();
+    }
+
+
+#endif
+
+
+}
